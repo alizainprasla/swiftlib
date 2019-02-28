08abd93b58289023d32eeab87c04a6fd90176203
Ali Zain <azain@foreepay.com>
2019-02-28 14:47:48 +0500

    initial commit

---
+++ SwiftLib/SwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by Ali Zain on 28/02/2019.
+//  Copyright © 2019 ForeePay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
