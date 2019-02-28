39a606551ff1d0dd3e4d50fd03164f11a9e1e497
Ali Zain <azain@foreepay.com>
2019-02-28 17:18:55 +0500

    Added test cases

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Ali Zain on 28/02/2019.
+//  Copyright © 2019 ForeePay. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func newa() -> String{
+        return "a"
+    }
+    
+}
--- SwiftLibTests/SwiftLibTests.swift
+++ SwiftLibTests/SwiftLibTests.swift
@@ -10,13 +10,28 @@ import XCTest
 @testable import SwiftLib
 
 class SwiftLibTests: XCTestCase {
+    
+    var swiftLib:SwiftyLib!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftLib = SwiftyLib()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testAdd(){
+        XCTAssertEqual(swiftLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftLib.sub(a: 2, b: 1), 1)
+    }
+    
+    func testNew(){
+        XCTAssertEqual(swiftLib.newa(), "a")
     }
 
     func testExample() {
